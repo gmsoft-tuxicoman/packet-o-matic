e400dd9f1d30b5a2b55e8131c789248ea2ab00e9
Guy Martin <gmsoft@tuxicoman.be>
2006-09-29 19:52:59 +0000

    Add header size checking for match_rtp.

--- match_rtp.c
+++ match_rtp.c
@@ -30,24 +30,39 @@ int match_config_rtp(struct match *m, void *params) {
 int match_eval_rtp(struct match* match, void *frame, unsigned int start, unsigned int len) {
 	struct rtphdr *hdr = frame + start;
 
+	if (len < 12) {
+		ndprint("Invalid size for RTP packet\n");
+		return 0;
+	}
+
 	
-	ndprint("Processing RTP packet -> SSRC : %x | Payload type %u | SEQ : %04x", hdr->ssrc, hdr->payload_type, ntohs(hdr->seq_num));
-#ifdef NDEBUG
-	int i;
-	for (i = 0; i < hdr->csrc_count; i++)
-		ndprint(" | CSRC : %x", hdr->csrc[i]);
-#endif
 
 	match->next_layer = -1; // Nothing else besides payload in RTP
 	int hdr_len;
 	hdr_len = 12; // Len up to ssrc included
 	hdr_len += hdr->csrc_count * 4;
 
+	if (len < hdr_len) {
+		ndprint("Invalid size for RTP packet\n");
+		return 0;
+	}
+
+	ndprint("Processing RTP packet -> SSRC : %x | Payload type %u | SEQ : %04x", hdr->ssrc, hdr->payload_type, ntohs(hdr->seq_num));
+#ifdef NDEBUG
+	int i;
+	for (i = 0; i < hdr->csrc_count && (hdr->csrc[i] + 4) < (frame + len); i++) {
+		ndprint(" | CSRC : %x", hdr->csrc[i]);
+	}
+#endif
 	if (hdr->extension) {
 		struct rtphdrext *ext;
 		ext = frame + start + hdr_len;
 		hdr_len += ntohs(ext->length);
 		ndprint(" | Extension header %u bytes", ntohs(ext->length));
+		if (len < hdr_len) {
+			ndprint(" Invalid size for RTP packet\n");
+			return 0;
+		}
 	}
 	match->next_start = start + hdr_len;
 	match->next_size = len - (match->next_start);
