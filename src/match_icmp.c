beedc1b9fc3f91a3cced5f7b1df78621491e8d2c
Guy Martin <gmsoft@tuxicoman.be>
2007-03-26 17:24:28 +0000

    Added sequence info and some type translation for echo.

--- src/match_icmp.c
+++ src/match_icmp.c
@@ -23,7 +23,7 @@
 
 int match_ipv4_id;
 struct match_functions *m_functions;
-struct layer_info *match_type_info, *match_code_info;
+struct layer_info *match_type_info, *match_code_info, *match_seq_info;
 
 int match_register_icmp(struct match_reg *r, struct match_functions *m_funcs) {
 
@@ -34,7 +34,9 @@ int match_register_icmp(struct match_reg *r, struct match_functions *m_funcs) {
 	match_ipv4_id = (*m_functions->match_register) ("ipv4");
 
 	match_type_info = (*m_funcs->layer_info_register) (r->match_type, "type", LAYER_INFO_TYPE_UINT32 | LAYER_INFO_PRINT_ZERO);
+	match_type_info->snprintf = match_layer_info_snprintf_icmp;
 	match_code_info = (*m_funcs->layer_info_register) (r->match_type, "code", LAYER_INFO_TYPE_UINT32);
+	match_seq_info = (*m_funcs->layer_info_register) (r->match_type, "seq", LAYER_INFO_TYPE_UINT32);
 
 	return 1;
 }
@@ -52,13 +54,34 @@ int match_identify_icmp(struct layer* l, void* frame, unsigned int start, unsign
 	switch (ihdr->type) {
 		case ICMP_ECHOREPLY:
 		case ICMP_ECHO:
+			match_seq_info->val.ui32 = ntohs(ihdr->un.echo.sequence);
+			return -1;
+
 		case ICMP_TIMESTAMP:
 		case ICMP_TIMESTAMPREPLY:
 		case ICMP_INFO_REQUEST:
 		case ICMP_INFO_REPLY:
+			match_seq_info->val.ui32 = 0;
 			return -1;
 	}
 
+	match_seq_info->val.ui32 = 0;
 	return  match_ipv4_id;
+}
+
+int match_layer_info_snprintf_icmp(char *buff, unsigned int len, struct layer_info *inf) {
+
+
+        switch (match_type_info->val.ui32) {
+                case ICMP_ECHO:
+                        strncpy(buff, "ping", len);
+                        return 4;
+                case ICMP_ECHOREPLY:
+                        strncpy(buff, "pong", len);
+                        return 4;
+        }
+
+        return snprintf(buff, len, "%u", match_type_info->val.ui32);
+
 }
 
