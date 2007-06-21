9766daceb6408ef4ee34fca4ca701d84a4300ca6
Guy Martin <gmsoft@tuxicoman.be>
2007-06-21 09:06:36 +0000

    Adding missing main.h file :)

---
+++ src/main.h
@@ -0,0 +1,32 @@
+/*
+ *  packet-o-matic : modular network traffic processor
+ *  Copyright (C) 2007 Guy Martin <gmsoft@tuxicoman.be>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+struct input_thread_params {
+	struct input *i; ///< The input we want to read from
+	int input_is_live; ///< If the input isn't live, we don't need to drop packets in case of overflow in buffer
+};
+
+int get_current_input_time(struct timeval *cur_time);
+
+#endif
