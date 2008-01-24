8b0fac0f2424d980cc7cf65c8568a628f07f161f
Guy Martin <gmsoft@tuxicoman.be>
2008-01-24 07:54:19 +0000

    Updated/fixed copyright dates.

--- src/rules.c
+++ src/rules.c
@@ -1,6 +1,6 @@
 /*
  *  packet-o-matic : modular network traffic processor
- *  Copyright (C) 2006-2007 Guy Martin <gmsoft@tuxicoman.be>
+ *  Copyright (C) 2006-2008 Guy Martin <gmsoft@tuxicoman.be>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
